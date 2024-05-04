b1cbf70b0de03d71a1e8a0555abdd5884fe8751b
Kutub Udd <socialnetwork98710@gmail.com>
2024-05-04 09:34:46 +0600

    added basic setup shadcn tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todo</h1>
+      <Button>MY app</Button>
+    </div>
+  );
+}
+
+export default App;
