e5d4a0ebcaf587445acdf93002245cd80bdb78df
Kutub Udd <socialnetwork98710@gmail.com>
2024-05-05 23:45:45 +0600

    added

--- src/Redux/api/baseApi.ts
+++ src/Redux/api/baseApi.ts
@@ -1,11 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { url } from 'inspector';
-const baseApi = createApi({
+
+export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
-  endpoints: (builder) => ({
-    getAllTask: (builder.mutation)=> ({
-    
-    })
-  })
+  endpoints: builder => ({
+    getAllTask: builder.query({
+      query: () => ({
+        url: '/task',
+        method: 'GET',
+      }),
+    }),
+  }),
 });
+
+export const { useGetAllTaskQuery } = baseApi;
--- src/components/Todo/TodoContainer.tsx
+++ src/components/Todo/TodoContainer.tsx
@@ -1,17 +1,16 @@
 import Container from '../ui/Container';
 import TodoCard from './TodoCard';
-import { TodoModal } from './TodaModal';
+
 import { TodoFilter } from './TodoFilter';
-import { useAppSelector } from '@/Redux/hooks';
+import { useGetAllTaskQuery } from '@/Redux/api/baseApi';
+import { TodoModal } from './TodaModal';
+import { TTodo } from '@/Redux/Features/TodoSlice';
 
 const TodoContainer = () => {
-  const todo = useAppSelector(state => state.todo.todo);
-  const sortedTodos = [...todo].sort((a, b) => {
-    if (a.isCompleted === b.isCompleted) return 0;
-    return a.isCompleted ? 1 : -1;
-  });
+  const { data, isError, isLoading } = useGetAllTaskQuery(undefined);
+
   return (
-    <Container className=" h-full text-center justify-center rounded-md shadow-md  w-full max-w-5xl mx-auto">
+    <Container className="h-full text-center justify-center rounded-md shadow-md w-full max-w-5xl mx-auto">
       <div className="flex justify-between bg-white px-3 py-2 rounded">
         <TodoModal />
         <div className="flex space-x-3 items-center">
@@ -19,17 +18,28 @@ const TodoContainer = () => {
           <TodoFilter />
         </div>
       </div>
-      <div className=" bg-primary-gradient p-2 space-y-1">
-        <div className="flex space-x-3 justify-between  px-3 py-2 bg-primary-gradient rounded text-white font-bold">
+      <div className="bg-primary-gradient p-2 space-y-1">
+        <div className="flex space-x-3 justify-between px-3 py-2 bg-primary-gradient rounded text-white font-bold">
           <input type="checkbox" />
           <h1>Title Name</h1>
           <h1>Description</h1>
-          {/* <h1>Time</h1> */}
           <h1>Status</h1>
           <h1>Action</h1>
         </div>
-        {sortedTodos.length > 0 ? (
-          todo.map(item => <TodoCard key={item.id} {...item} />)
+        {isLoading && (
+          <div className="bg-white flex space-x-3 text-center ">
+            <span className="animate-spin">...</span>
+            <span className="">Data is loading</span>
+          </div>
+        )}
+        {isError && (
+          <div className="bg-white flex space-x-3">
+            <span className="animate-spin">...</span>
+            <span>Something went wrong</span>
+          </div>
+        )}
+        {data?.length > 0 ? (
+          data.map((item: TTodo) => <TodoCard key={item.id} {...item} />)
         ) : (
           <div className="bg-white text-center text-2xl text-red-600 mx-w-2xl mx-auto p-4 rounded font-bold">
             <h1>There is no todolist to do !</h1>
